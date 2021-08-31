bf7544e69b550273d000027074712d6a63604c83
Michael Tarassov <michael@tarassov.me>
2021-08-31 21:14:08 +0300

    Render results as a JSON array

--- render.go
+++ render.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"text/tabwriter"
@@ -22,4 +23,33 @@ func renderTable(w io.Writer, results []scan.Result) {
 		fmt.Fprintf(tw, "%s\t%s\t%s\t%d\t%s\n", r.Target.Addr(), state, latency, r.Attempts, detail)
 	}
 	tw.Flush()
+}
+
+type jsonResult struct {
+	Host      string  `json:"host"`
+	Port      int     `json:"port"`
+	Open      bool    `json:"open"`
+	LatencyMS float64 `json:"latency_ms"`
+	Attempts  int     `json:"attempts"`
+	Error     string  `json:"error,omitempty"`
+}
+
+func renderJSON(w io.Writer, results []scan.Result) error {
+	out := make([]jsonResult, 0, len(results))
+	for _, r := range results {
+		jr := jsonResult{
+			Host:     r.Target.Host,
+			Port:     r.Target.Port,
+			Open:     r.Open,
+			Attempts: r.Attempts,
+			Error:    r.Err,
+		}
+		if r.Open {
+			jr.LatencyMS = float64(r.Latency.Microseconds()) / 1000.0
+		}
+		out = append(out, jr)
+	}
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "  ")
+	return enc.Encode(out)
 }
